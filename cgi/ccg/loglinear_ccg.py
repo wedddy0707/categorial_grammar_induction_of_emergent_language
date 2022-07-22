24050ec9001fdae42f1a6d793a4480877ff34d8a
wedddy0707 <ryoryo.ueda@gmail.com>
2022-07-22 15:37:24 +0900

    introduce heap sort for beam CKY delete unnecessary codes add apply_transduction function

--- cgi/ccg/loglinear_ccg.py
+++ cgi/ccg/loglinear_ccg.py
@@ -1,14 +1,14 @@
+import heapq
+import copy
 import itertools
-import random
 from collections import Counter, defaultdict
-from typing import (Callable, Dict, Hashable, List, Optional, Sequence, Set,
-                    Tuple, Union)
+from typing import Callable, Dict, Hashable, List, Optional, Set, Tuple, Union
 
 import numpy as np
 import numpy.typing as npt
 
 from .cell import Derivation
-from .lexitem import BasicCat, LexItem, Sem
+from .lexitem import BasicCat, LexItem, Sem, TransductionRule, Var
 
 
 class NoDerivationObtained(Exception):
@@ -56,6 +56,7 @@ class LogLinearCCG:
     __lexicon: Lexicon
     __pho_to_lexicon: "defaultdict[Tuple[Hashable, ...], Set[Derivation]]"
     __unigram: "defaultdict[Hashable, float]"
+    __lexitem_to_transduction_rule: Dict[LexItem, TransductionRule]
     derivations: List[Derivation]
     parseable_sentences: List[Sentence]
 
@@ -74,6 +75,7 @@ class LogLinearCCG:
         self.__lexicon = set()
         self.__pho_to_lexicon = defaultdict(set)
         self.__unigram = defaultdict(float)
+        self.__lexitem_to_transduction_rule = dict()
         self.step = 0
 
     @property
@@ -90,7 +92,9 @@ class LogLinearCCG:
             self.__pho_to_lexicon[e.pho].add(
                 Derivation(
                     item=e,
-                    score=self.params[e]))
+                    score=self.params[e],
+                ),
+            )
 
     def update_lexicon(self, x: Union[Lexicon, LexItem]):
         if isinstance(x, LexItem):
@@ -98,14 +102,18 @@ class LogLinearCCG:
             self.__pho_to_lexicon[x.pho].add(
                 Derivation(
                     item=x,
-                    score=self.params[x]))
+                    score=self.params[x],
+                ),
+            )
         else:
             for e in x - self.__lexicon:
                 self.__lexicon.add(e)
                 self.__pho_to_lexicon[e.pho].add(
                     Derivation(
                         item=e,
-                        score=self.params[e]))
+                        score=self.params[e],
+                    ),
+                )
 
     @property
     def unigram(self):
@@ -119,35 +127,41 @@ class LogLinearCCG:
         for k, v in x.items():
             self.__unigram[k] = v / normalizer
 
-    def unigram_model(self, x: Union[int, Sequence[int]]) -> float:
-        if isinstance(x, int):
-            x = [x]
+    def unigram_model(self, x: Sentence) -> float:
         return float(np.prod([self.unigram[e] for e in x]))
 
     def zero_grad(self):
         self.derivations = []
         self.grad = defaultdict(float)
 
+    def prob_of_target_logical_form(
+        self,
+        derivations: List[Derivation],
+        target_logical_form: Sem,
+        probs: Optional[npt.NDArray[np.float_]] = None,
+    ):
+        logical_forms = np.array([x.item.sem for x in derivations])
+        if probs is None:
+            probs = np_softmax(np.array([x.score for x in derivations]))
+        return np.sum(probs[logical_forms == target_logical_form])
+
     def calc_grad(
         self,
         derivations: List[Derivation],
         target_logical_form: Sem,
     ):
-        logical_forms = np.array([x.item.sem for x in derivations], dtype=Sem)
         features = [Counter(x.lexitems) for x in derivations]
-        ps: npt.NDArray[np.float_] = np_softmax(
+        probs: npt.NDArray[np.float_] = np_softmax(
             np.array([x.score for x in derivations], dtype=np.float_)
         )
-
-        are_correct: npt.NDArray[np.bool_] = logical_forms == target_logical_form
-
-        p_to_be_correct: float = np.sum(ps[are_correct])
-
-        for p, f, is_correct in zip(ps, features, are_correct):
+        prob_of_target_logical_form = self.prob_of_target_logical_form(
+            derivations, target_logical_form, probs=probs,
+        )
+        for p, f, derivation in zip(probs, features, derivations):
             for k, v in f.items():
                 self.grad[k] -= v * p
-                if is_correct:
-                    self.grad[k] += v * p / p_to_be_correct
+                if derivation.item.sem == target_logical_form:
+                    self.grad[k] += v * p / prob_of_target_logical_form
 
     def update_params(self):
         lr_here = self.lr / (1 + self.c * self.step)
@@ -161,14 +175,12 @@ class LogLinearCCG:
         lexicon: Optional[Lexicon] = None,
         logical_form: Optional[Sem] = None,
         beam_size: Optional[int] = None,
-        return_only_top_score: bool = False,
     ):
         return self.parse(
             sentence,
             lexicon,
             logical_form=logical_form,
             beam_size=beam_size,
-            return_only_top_score=return_only_top_score,
         )
 
     def parse(
@@ -177,124 +189,98 @@ class LogLinearCCG:
         lexicon: Optional[Lexicon] = None,
         logical_form: Optional[Sem] = None,
         beam_size: Optional[int] = None,
-        return_only_top_score: bool = False,
     ):
         assert lexicon or self.lexicon, lexicon
 
-        if not lexicon:
-            lexicon = self.lexicon
+        if lexicon is None:
             pho_to_lexicon = self.__pho_to_lexicon
         else:
-            pho_to_lexicon: 'defaultdict[Tuple[Hashable, ...], Set[Derivation]]' = defaultdict(set)  # noqa: E501
-            for e in lexicon:
+            pho_to_lexicon: Dict[Tuple[Hashable, ...], Set[Derivation]] = {}
+            for e in sorted(lexicon):
+                if e.pho not in pho_to_lexicon:
+                    pho_to_lexicon[e.pho] = set()
                 pho_to_lexicon[e.pho].add(
                     Derivation(
                         item=e,
-                        score=self.params[e]))
+                        score=self.params[e],
+                    )
+                )
 
-        def prune_fn(
-            x: List[Derivation],
-            b: Optional[int] = beam_size,
-        ) -> List[Derivation]:
-            if b is None or len(x) < b:
-                return x
-            else:
-                return sorted(
-                    x, key=(lambda k: k.score), reverse=True)[:b]
-
-        cell: 'defaultdict[Tuple[int, int], List[Derivation]]' = defaultdict(list)  # noqa: E501
+        cell: "defaultdict[Tuple[int, int], List[Derivation]]" = defaultdict(list)
 
         for width in range(len(sentence) + 1):
             for i in range(len(sentence) - width + 1):
                 j = i + width
 
-                items = sorted(
-                    pho_to_lexicon[sentence[i:j]], key=(lambda x: str(x)))
-                items = random.sample(items, len(items))
-                cell[i, j].extend(items)
+                for item in pho_to_lexicon[sentence[i:j]]:
+                    heapq.heappush(cell[i, j], item)
 
                 for k in range(i + 1, j):
-                    derivs_1 = prune_fn(cell[i, k])
-                    derivs_2 = prune_fn(cell[k, j])
-                    for deriv_1 in derivs_1:
-                        for deriv_2 in derivs_2:
-                            if deriv_1.item.can_take_as_right_arg(deriv_2.item):  # noqa: E501
-                                item = deriv_1.item.takes_as_right_arg(deriv_2.item)  # noqa: E501
-                                cell[i, j].append(
+                    derivs_1 = copy.deepcopy(cell[i, k])
+                    derivs_2 = copy.deepcopy(cell[k, j])
+                    beam_size_1 = min(len(derivs_1), (len(derivs_1) if beam_size is None else beam_size))
+                    beam_size_2 = min(len(derivs_2), (len(derivs_2) if beam_size is None else beam_size))
+                    beam_1 = [heapq.heappop(derivs_1) for _ in range(beam_size_1)]
+                    beam_2 = [heapq.heappop(derivs_2) for _ in range(beam_size_2)]
+                    for deriv_1 in beam_1:
+                        for deriv_2 in beam_2:
+                            if deriv_1.item.can_take_as_right_arg(deriv_2.item):
+                                item = deriv_1.item.takes_as_right_arg(deriv_2.item)
+                                heapq.heappush(
+                                    cell[i, j],
                                     Derivation(
                                         item=item,
                                         score=(deriv_1.score + deriv_2.score),
-                                        backptrs=(deriv_1, deriv_2)))
-                            elif deriv_2.item.can_take_as_left_arg(deriv_1.item):  # noqa: E501
-                                item = deriv_2.item.takes_as_left_arg(deriv_1.item)  # noqa: E501
-                                cell[i, j].append(
+                                        backptrs=(deriv_1, deriv_2),
+                                    ),
+                                )
+                            elif deriv_2.item.can_take_as_left_arg(deriv_1.item):
+                                item = deriv_2.item.takes_as_left_arg(deriv_1.item)
+                                heapq.heappush(
+                                    cell[i, j],
                                     Derivation(
                                         item=item,
                                         score=(deriv_1.score + deriv_2.score),
-                                        backptrs=(deriv_1, deriv_2)))
+                                        backptrs=(deriv_1, deriv_2),
+                                    ),
+                                )
         derivs = cell[0, len(sentence)]
         derivs = filter(lambda x: x.item.cat == BasicCat.S, derivs)
         if logical_form is not None:
             derivs = filter(lambda x: x.item.sem == logical_form, derivs)
-        if return_only_top_score:
-            derivs = list(derivs)  # this operation is necessary.
-            if len(derivs) > 0:
-                top_score = max(p.score for p in derivs)
-                derivs = filter(lambda x: x.score == top_score, derivs)
         return list(derivs)
 
-    def set_parseable_sentences(
+    def apply_transduction(
         self,
-        vocab_size: int,
-        sentence_len: int,
-        lexicon: Optional[Lexicon] = None,
-    ):
-        assert isinstance(vocab_size, int) and vocab_size > 0, vocab_size
-        assert isinstance(sentence_len, int) and sentence_len > 0, sentence_len
-        self.parseable_sentences = []
-        for sentence in itertools.product(range(1, vocab_size),
-                                          repeat=sentence_len):
-            parses = self(sentence, lexicon)
-            if len(parses) > 0:
-                self.parseable_sentences.append(sentence)
-        return self.parseable_sentences
+        sem: Sem,
+    ) -> Tuple[List[Sentence], List[List[LexItem]]]:
+        sentences: List[Sentence] = []
+        histories: List[List[LexItem]] = []
+        for lexitem in self.lexicon:
+            try:
+                rule = self.__lexitem_to_transduction_rule[lexitem]
+            except KeyError:
+                rule = self.__lexitem_to_transduction_rule[lexitem] = lexitem.to_transduction_rule()
 
-    def generate(
-        self,
-        logical_form: Sem,
-        vocab_size: int,
-        sentence_len: int,
-        n_samples: int = 10000,
-        lexicon: Optional[Lexicon] = None,
-        append_eos: bool = True,
-    ):
-        # assert hasattr(self, 'parseable_sentences')
-        assert isinstance(logical_form, Sem), logical_form
-        assert isinstance(append_eos, bool), append_eos
-
-        vocab = tuple(range(1, vocab_size))
-        probs = tuple(self.__unigram[e] for e in vocab)
-
-        sentence = (0,) * sentence_len
-        max_prob = 0.0
-
-        for _ in range(n_samples):
-            candidate = tuple(
-                random.choices(vocab, k=sentence_len, weights=probs))
-            prior_prob = self.unigram_model(candidate)
-            if prior_prob < max_prob:
-                continue
-            parses = self(candidate, lexicon)
-
-            sems = np.array([x.item.sem for x in parses], dtype=Sem)
-            likelihoods = np_softmax(np.array([x.score for x in parses], dtype=np.float_))
-            likelihood = np.sum(likelihoods[sems == logical_form])
-
-            joint_prob = likelihood * prior_prob
-            if joint_prob > max_prob:
-                sentence = candidate
-
-        if append_eos:
-            sentence = sentence + (0,)
-
-        return sentence
+            var_to_sub_sem = rule.lhs.get_map_from_var_to_subsumed_sem(sem)
+            if var_to_sub_sem is not None:
+                variable_substituted_rhs = (
+                    var_to_sub_sem[x] if isinstance(x, Var) else x
+                    for x in rule.rhs
+                )
+                sub_sentences_and_histories: List[Tuple[List[Sentence], List[List[LexItem]]]] = [
+                    self.apply_transduction(x)
+                    if isinstance(x, Sem) else ([x], [[]])
+                    for x in variable_substituted_rhs
+                ]
+                sub_sentences = map(lambda x: x[0], sub_sentences_and_histories)
+                sub_histories = map(lambda x: x[1], sub_sentences_and_histories)
+                all_sentences: List[Sentence] = [
+                    sum(x, start=()) for x in itertools.product(*sub_sentences)
+                ]
+                all_histories: List[List[LexItem]] = [
+                    sum(x, start=[lexitem]) for x in itertools.product(*sub_histories)
+                ]
+                sentences.extend(all_sentences)
+                histories.extend(all_histories)
+        return sentences, histories
