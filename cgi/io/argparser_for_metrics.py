e57604f3bf9a53a610b90ed7c3207d6e1ef24402
wedddy0707 <ryoryo.ueda@gmail.com>
2022-09-02 14:41:25 +0900

    remove AdjSwap from target langs

--- cgi/io/argparser_for_metrics.py
+++ cgi/io/argparser_for_metrics.py
@@ -49,7 +49,7 @@ def get_params(
                             "emergent",
                             "shuffled",
                             "random",
-                            "adjacent_swapped",
+                            # "adjacent_swapped",
                         ],
                         choices=(
                             "input",
